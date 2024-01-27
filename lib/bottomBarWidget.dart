e0d8c5209cafd05b8cf4e5b0e9889a65376a59d5
Ege Yardimci <egeyardimci2003@gmail.com>
2024-01-27 16:22:29 +0300

    added widget files

---
+++ lib/bottomBarWidget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BottomBarWidget extends StatelessWidget {
+  BottomBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
