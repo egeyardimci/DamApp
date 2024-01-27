d6fb44b608dac651e24a11b298399fcc8dc2cffd
Ege Yardimci <egeyardimci2003@gmail.com>
2024-01-27 15:40:35 +0300

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,18 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:agaol/quote.dart';
+import 'package:agaol/loading.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import "quoteWidget.dart";
+import 'package:agaol/homeWidget.dart';
 
-void main() => runApp(MyApp());
-
-
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeWidget(),
-      },
-    );
-  }
-}
-
-class HomeWidget extends StatefulWidget {
-  HomeWidget({super.key});
-
-  @override
-  State<HomeWidget> createState() => _HomeWidgetState();
-}
-
-class _HomeWidgetState extends State<HomeWidget> {
-  int level = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text(
-          "DamApp",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold
-          ),
-        ),
-        actions: [
-          IconButton(
-            color: Colors.white,
-            icon: Icon(Icons.favorite),
-            onPressed: () {
-              // Add action for the settings button
-              print('Settings button pressed');
-            },
-          ),
-          IconButton(
-            color: Colors.white,
-            icon: Icon(Icons.message),
-            onPressed: () {
-              // Add action for the settings button
-              print('Settings button pressed');
-            },
-
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            backgroundColor: Colors.amber,
-            icon: Icon(Icons.add),
-            label: 'Add',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-          BottomNavigationBarItem(
-            backgroundColor: Colors.amber,
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-
-
-
+void main() => runApp(MaterialApp(
+  initialRoute: '/loading',
+  routes: {
+    '/loading': (context) => LoadingWidget(),
+    '/home': (context) => HomeWidget()
+  },
+));
 
 
 
