29c80c531cf00ee0d0ebb88f591dbd74531aa05d
Your Name <you@example.com>
2024-01-27 16:03:57 +0300

    ad

--- lib/profileWidget.dart
+++ lib/profileWidget.dart
@@ -11,6 +11,98 @@ class ProfileWidget extends StatefulWidget {
 class _ProfileWidgetState extends State<ProfileWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[50],
+      appBar: AppBar(
+        title: Text("DAM PROFILE",
+          style: TextStyle(
+            color: Colors.lightBlueAccent,
+            letterSpacing: 1.5,
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.lightBlue[900],
+        elevation: 0.0,
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.lightBlue[900],
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundImage: AssetImage("assets/me.jpg"),
+              radius: 40.0,
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.lightBlue[900],
+            ),
+            Text("Name:",
+              style: TextStyle(
+                color: Colors.lightBlue[900],
+                letterSpacing: 1.5,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text("Age: e",
+              style: TextStyle(
+                color: Colors.lightBlue[900],
+                letterSpacing: 1.5,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text("Gender:",
+              style: TextStyle(
+                color: Colors.lightBlue[900],
+                letterSpacing: 1.5,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text("Dating Preferance:",
+              style: TextStyle(
+                color: Colors.lightBlue[900],
+                letterSpacing: 1.5,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email_rounded,
+                  color: Colors.grey[500],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  "dam_bulmakistiyorum@gmail.com",
+                  style: TextStyle(
+                    color: Colors.black,
+                    letterSpacing: 1.5,
+                    fontSize: 15.0,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
