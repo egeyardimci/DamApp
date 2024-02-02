86d66b51f4de6d6e68aafda74e992b2c178db061
Ege Yardimci <egeyardimci2003@gmail.com>
2024-02-03 00:40:33 +0300

    settings page edit

--- lib/App/settingsWidget.dart
+++ lib/App/settingsWidget.dart
@@ -1,4 +1,5 @@
 import 'package:agaol/Auth/authService.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:agaol/App/bottomBarWidget.dart';
 import 'package:agaol/App/topBarWidget.dart';
@@ -18,11 +19,111 @@ class _SettingsWidgetState extends State<SettingsWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TopBarWidget(title: 'DamApp',),
-      body: ElevatedButton(
-        onPressed: () async {
-          await _auth.signOut();
-        },
-        child: Text("Log Out"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            children: [
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(),
+                  child: ExpansionTile(
+                    title: ListTile(
+                      leading: Icon(Icons.timelapse_sharp),
+                      title: Text("Age"),
+                    ),
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                labelText: 'New Age',
+                              ),
+                              onChanged: (value) {
+                                // Handle the minimum age input value
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(),
+                  child: ExpansionTile(
+                    title: ListTile(
+                      leading: Icon(Icons.transgender),
+                      title: Text("Gender"),
+                    ),
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              keyboardType: TextInputType.text,
+                              decoration: InputDecoration(
+                                labelText: 'New Gender',
+                              ),
+                              onChanged: (value) {
+                                // Handle the minimum age input value
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(),
+                  child: ExpansionTile(
+                    title: ListTile(
+                      leading: Icon(Icons.supervised_user_circle_sharp),
+                      title: Text("Preference"),
+                    ),
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              keyboardType: TextInputType.text,
+                              decoration: InputDecoration(
+                                labelText: 'New Preference',
+                              ),
+                              onChanged: (value) {
+                                // Handle the minimum age input value
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () async {
+                await _auth.signOut();
+              },
+              child: Text("Log Out"),
+            ),
+          ),
+
+
+        ]
       ),
       bottomNavigationBar: BottomBarWidget(),
     );
