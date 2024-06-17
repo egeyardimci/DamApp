161c5305146ff6f2c5602cea6ac8092bc5ea83a3
aslihanhamzaoglu <aslihan.hamzaoglu@sabanciuniv.edu>
2024-06-18 01:37:08 +0300

    username check dummy commit

--- lib/Database/requestDatabase.dart
+++ lib/Database/requestDatabase.dart
@@ -1,7 +1,6 @@
 
 import 'package:agaol/Auth/authService.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 import '../Models/requestModel.dart';
 import '../Models/userModel.dart';
